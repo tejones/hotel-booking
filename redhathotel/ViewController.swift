1c67b02bb9ed04955bf723c209df17dac0444548
Ted Jones <tejones@redhat.com>
2018-04-10 12:16:24 -0500

    Initial Commit

---
+++ redhathotel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  redhathotel
+//
+//  Created by Ted Jones - Red Hat on 4/10/18.
+//  Copyright © 2018 Ted Jones - Red Hat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
