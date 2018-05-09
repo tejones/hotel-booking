18a2a6716a0758dbdfab203a62b5f4adb26ac935
Ted Jones <tejones@redhat.com>
2018-05-09 10:39:06 -0500

    Changes for microservice architecture

--- hotel-booking-microservice/reservation-service/src/main/java/com/redhat/hotelbooking/repository/SourceReservationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.redhat.hotelbooking.repository;
-
-import com.redhat.hotelbooking.bean.SourceReservation;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SourceReservationRepository extends CrudRepository<SourceReservation, Integer> {
-}
--- mobile-app/ios/redhathotel/Pods/Target Support Files/Pods-redhathotelUITests/Pods-redhathotelUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_redhathotelUITests : NSObject
-@end
-@implementation PodsDummy_Pods_redhathotelUITests
-@end
